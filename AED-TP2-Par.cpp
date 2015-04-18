14dfdc8b295b3b305a7bd05d6047d28821dbd771
nahuel-decampos <nahuel.decampos@gmail.com>
2015-04-18 15:08:31 -0300

    Create AED-TP2-Par.cpp

---
+++ AED-TP2-Par.cpp
@@ -0,0 +1,24 @@
+/* TP 2: Número par
+   Nahuel de Campos
+   18/04/15
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\nTRABAJO PRACTICO #2: Numero par\n";
+    cout << "------------------------------------------------------------\n";
+
+    int a;
+    cout << "Ingrese un numero entero\n\n";
+    cin >> a;
+    bool par = !(a % 2);
+
+    if (par)
+        cout << "\nEl valor " << a << " es par \n\n";
+    else
+        cout << "\nEl valor " << a << " no es par \n\n";
+}
